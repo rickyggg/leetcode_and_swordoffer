abe3c2af1dad500520e5c90873d2b55f6670bf24
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-16 10:53:44 +0800

    add solution 876

---
+++ src/876_middle_of_the_linked_list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  ListNode *middleNode(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast->next && fast->next->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    if (fast->next)
+      return slow->next;
+    else
+      return slow;
+  }
+};
