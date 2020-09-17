c5d8faf53174d323e249a1b936be1349c7aa34bf
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-17 11:26:48 +0800

    add solution 14

---
+++ src/14_longest_common_prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  string longestCommonPrefix(vector<string> &strs) {
+    string prefix = "";
+    int n = strs.size();
+    if (n == 0)
+      return prefix;
+    for (int i = 0; i < strs[0].size(); ++i) {
+      int j = 1;
+      while (j < n && strs[j].size() > i) {
+        if (strs[j][i] != strs[0][i])
+          return prefix;
+        ++j;
+      }
+      if (j == strs.size())
+        prefix += strs[0][i];
+    }
+    return prefix;
+  }
+};
