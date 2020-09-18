7fd7e37142236ce94328445e2989cf22a7011d66
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-18 12:43:32 +0800

    add solution 8

---
+++ src/8_atoi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+  int myAtoi(string str) {
+    if (str.size() == 0)
+      return 0;
+    int index = 0;
+    for (int i = 0; i < str.size(); i++) {
+      if (str[i] == ' ')
+        index++;
+      else
+        break;
+    }
+    int result = 0;
+    bool isNeg = false;
+    if (str[index] == '-')
+      isNeg = true;
+    if (str[index] == '-' || str[index] == '+')
+      index++;
+    for (int i = index; i < str.size(); i++) {
+      if (str[i] > '9' || str[i] < '0')
+        break;
+      int currNum = str[i] - '0';
+      if (result > (INT_MAX / 10) || result * 10 > INT_MAX - currNum)
+        return isNeg ? INT_MIN : INT_MAX;
+      result = result * 10 + currNum;
+    }
+    return isNeg ? result * (-1) : result;
+  }
+};
