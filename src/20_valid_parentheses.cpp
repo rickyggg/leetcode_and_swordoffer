76b7f0237617e795fdc32cb09ce74b73e1a24241
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-22 10:34:32 +0800

    add solution 20

---
+++ src/20_valid_parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  bool isValid(string s) {
+    stack<char> st;
+    for (const char &c : s) {
+      if (c == '(' || c == '{' || c == '[') {
+        st.push(c);
+      } else {
+        if (st.empty())
+          return false;
+        if (c == ')' && st.top() != '(')
+          return false;
+        if (c == '}' && st.top() != '{')
+          return false;
+        if (c == ']' && st.top() != '[')
+          return false;
+        st.pop();
+      }
+    }
+    return st.empty();
+  }
+};
