1fdf67b1f714da621617c24da5159064386f5f94
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-28 09:56:52 +0800

    add solution 101

---
+++ src/101_symmetric_tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  bool isSymmetric(TreeNode *root) { return isMirror(root, root); }
+  bool isMirror(TreeNode *t1, TreeNode *t2) {
+    if (t1 == nullptr && t2 == nullptr)
+      return true;
+    if (t1 == nullptr || t2 == nullptr)
+      return false;
+    return (t1->val == t2->val && isMirror(t1->right, t2->left) &&
+            isMirror(t1->left, t2->right));
+  }
+};
