56a21ca9fed763963d6bfe1ec1199f3ae7b149cf
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-09-30 10:13:06 +0800

    make it easy to understand

--- src/21_merge_two_sorted_lists.cpp
+++ src/21_merge_two_sorted_lists.cpp
@@ -31,7 +31,8 @@ public:
       return l2;
     } else if (l2 == nullptr) {
       return l1;
-    } else if (l1->val < l2->val) {
+    }
+    if (l1->val < l2->val) {
       l1->next = mergeTwoLists(l1->next, l2);
       return l1;
     } else {
