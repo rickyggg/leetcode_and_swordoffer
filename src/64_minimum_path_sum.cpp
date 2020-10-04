bfb1b81f1d4903f3ee8938e3f4e79fcb26be08f4
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-04 14:33:27 +0800

    add solution 64

---
+++ src/64_minimum_path_sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  /*
+  int minPathSum(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    if (m <= 0 || n <= 0)
+      return 0;
+    int dp[m][n];
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < m; ++i)
+      dp[i][0] = dp[i - 1][0] + grid[i][0];
+    for (int i = 1; i < n; ++i)
+      dp[0][i] = dp[0][i - 1] + grid[0][i];
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j)
+        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+    return dp[m - 1][n - 1];
+  }
+  */
+  int minPathSum(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    if (m <= 0 || n <= 0)
+      return 0;
+    int dp[n];
+    dp[0] = grid[0][0];
+    for (int i = 1; i < n; ++i)
+      dp[i] = dp[i - 1] + grid[0][i];
+    for (int i = 1; i < m; ++i) {
+      dp[0] += grid[i][0];
+      for (int j = 1; j < n; ++j) {
+        dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
+      }
+    }
+    return dp[n - 1];
+  }
+};
