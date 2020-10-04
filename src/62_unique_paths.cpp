239dbe24c92e1c10dc36311fb54cdcd69116bccb
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-04 14:09:24 +0800

    add solution 62

--- src/62_unique_paths.cpp
+++ src/62_unique_paths.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-  int uniquePaths(int m, int n) {
-    int dp[m][n];
-    for (int i = 0; i < m; ++i)
-      dp[i][0] = 1;
-    for (int i = 0; i < n; ++i)
-      dp[0][i] = 1;
-    for (int i = 1; i < m; ++i) {
-      for (int j = 1; j < n; ++j)
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+  /*
+    int uniquePaths(int m, int n) {
+      int dp[m][n];
+      for (int i = 0; i < m; ++i)
+        dp[i][0] = 1;
+      for (int i = 0; i < n; ++i)
+        dp[0][i] = 1;
+      for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j)
+          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+      return dp[m - 1][n - 1];
     }
-    return dp[m - 1][n - 1];
+  */
+  int uniquePaths(int m, int n) {
+    int dp[n];
+    for (int i = 0; i < n; ++i)
+      dp[i] = 1;
+    for (int i = 1; i < m; ++i) {
+      dp[0] = 1;
+      for (int j = 1; j < n; ++j)
+        dp[j] = dp[j - 1] + dp[j];
+    }
+    return dp[n - 1];
   }
 };
