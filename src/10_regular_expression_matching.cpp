3436f5277e443ca4631562d932f40db792819596
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-10-07 17:02:28 +0800

    add solution 10

--- src/10_regular_expression_matching.cpp
+++ src/10_regular_expression_matching.cpp
@@ -5,8 +5,8 @@ public:
       return s.empty();
     bool first_match = (!s.empty() && (p[0] == s[0] || p[0] == '.'));
     if (p.size() >= 2 && p[1] == '*')
-      return (isMatch(s, p.substr(2))) ||
-             (first_match && isMatch(s.substr(1), p));
+      return (first_match && isMatch(s.substr(1), p)) ||
+             (isMatch(s, p.substr(2)));
     else
       return first_match && isMatch(s.substr(1), p.substr(1));
   }
