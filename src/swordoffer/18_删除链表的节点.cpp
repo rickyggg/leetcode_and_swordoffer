e4cccfb768322b9c59913c950295e747d9743945
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-11-03 10:24:08 +0800

    add new solution 18 linked list

---
+++ src/swordoffer/18_删除链表的节点.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        if (!head) return head;
+        if (head->val == val)
+            return head->next ? head->next : head;
+        ListNode *p = head;
+        while (p->next) {
+            if (p->next->val == val) {
+                p->next = p->next->next;
+                break;
+            }
+            p = p->next;
+        }
+        return head;
+    }
+};
