35e103de951649d2b7ebde4a9031d194a57ba8b5
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-11-12 10:26:25 +0800

    add new solution 3 sliding window

--- src/leetcode/3_longest_substr_without_repeat.cpp
+++ src/leetcode/3_longest_substr_without_repeat.cpp
@@ -5,12 +5,10 @@ public:
     int left = 0, right = 0;
     int max_len = 0;
     while (right < s.size()) {
-      char r = s[right];
-      ++right;
+      char r = s[right++];
       ++m[r];
       while (m[r] > 1) {
-        char l = s[left];
-        ++left;
+        char l = s[left++];
         --m[l];
       }
       max_len = max(max_len, right - left);
