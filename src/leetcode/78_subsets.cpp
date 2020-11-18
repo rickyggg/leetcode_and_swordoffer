6486b0254778eb726102f72a704e966a86889b1d
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-11-18 10:07:06 +0800

    add the solution 78 backtrack

--- src/leetcode/78_subsets.cpp
+++ src/leetcode/78_subsets.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-  vector<vector<int>> subsets(vector<int> &S) {
-    vector<vector<int>> res(1, vector<int>());
-    sort(S.begin(), S.end());
-    for (int i = 0; i < S.size(); i++) {
-      int n = res.size();
-      for (int j = 0; j < n; j++) {
-        res.push_back(res[j]);
-        res.back().push_back(S[i]);
-      }
-    }
+  vector<vector<int>> subsets(vector<int> &nums) {
+    backtrack(nums, 0);
     return res;
+  }
+
+private:
+  vector<vector<int>> res;
+  vector<int> temp;
+
+  void backtrack(vector<int> &nums, int cur) {
+    if (cur == nums.size()) {
+      res.push_back(temp);
+      return;
+    }
+    temp.push_back(nums[cur]);
+    backtrack(nums, cur + 1);
+    temp.pop_back();
+    backtrack(nums, cur + 1);
   }
 };
