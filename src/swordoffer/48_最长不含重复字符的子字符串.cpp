9196370015100b7872ee8adefa3ca14608e08144
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-11-19 14:32:04 +0800

    add new solution 48 sliding windows

---
+++ src/swordoffer/48_最长不含重复字符的子字符串.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> m;
+    int left = 0, right = 0;
+    int max_len = 0;
+    while (right < s.size()) {
+        char r = s[right++];
+        ++m[r];
+        while (m[r] > 1) {
+            char l = s[left++];
+            --m[l];
+        }
+        max_len = max(max_len, right - left);
+    }
+    return max_len; 
+  }
+};
