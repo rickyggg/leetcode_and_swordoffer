62dd6dd4277b660d5d12db587bba2de8851be8aa
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-12-11 10:40:24 +0800

    add new solution 416 0-1 knapsack

---
+++ src/leetcode/416_分割等和子集.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int len = nums.size();
+        int sum = 0;
+        for (int &i : nums)
+            sum += i;
+        if ((sum & 1) == 1)   
+            return false;
+        int target = sum / 2;
+        bool dp[len][target + 1];
+        memset(dp, false, sizeof(dp));
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (nums[i] == j) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j)
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+            }
+        }
+        return dp[len - 1][target];
+    }
+};
