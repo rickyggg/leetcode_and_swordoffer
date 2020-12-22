ce6e9c0e25086ef4136b0ab9cefe597684097908
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-12-22 14:15:51 +0800

    add new solution 63 dp

---
+++ src/swordoffer/63_股票的最大利润.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2)  return 0;
+        int dp_i_0 = 0, dp_i_1 = INT_MIN;
+        for (int i = 0; i < prices.size(); ++i) {
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+};
