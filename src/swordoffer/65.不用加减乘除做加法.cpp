692d48decb497c1aa816b386b4fd8471086d0afe
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2020-12-25 12:17:59 +0800

    add new solution 65 math

---
+++ src/swordoffer/65.不用加减乘除做加法.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int add(int a, int b) {
+        int sum, carry;
+        while (b != 0) {
+            sum = a ^ b;
+            carry = (unsigned int)(a & b) << 1;
+            a = sum;        
+            b = carry;
+        }
+        return a;
+    }
+};
