f92e5cc8bb3c19dd0414a22e67289a3da79811a5
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2021-01-02 10:47:38 +0800

    add new solution 68 recursive

--- src/swordoffer/68_i_二叉搜索树的最近公共祖先.cpp
+++ src/swordoffer/68_i_二叉搜索树的最近公共祖先.cpp
@@ -9,13 +9,14 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || !p || !q) return nullptr;
-        if (p->val < root->val && q->val < root->val) 
-            return lowestCommonAncestor(root->left, p, q);
-        else if (p->val > root->val && q->val > root->val)
-            return lowestCommonAncestor(root->right, p, q);
-        else
-            return root;
-    }
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+    if (!root || !p || !q)
+      return nullptr;
+    if (p->val < root->val && q->val < root->val)
+      return lowestCommonAncestor(root->left, p, q);
+    else if (p->val > root->val && q->val > root->val)
+      return lowestCommonAncestor(root->right, p, q);
+    else
+      return root;
+  }
 };
